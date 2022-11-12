b9ec027fc892398fb9eda65588789f70dd73f083
정성우 <rnrn78@opensg.net>
2022-11-13 02:57:36 +0900

    스프링 부트 프로젝트 시작하기
    스프링 부트 프로젝트 초기화

---
+++ fastcampus-project-board/src/test/java/com/fastcampus/fastcampusboard/FastCampusProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
